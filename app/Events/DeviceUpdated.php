53159f78c8f12bc10a601adcb079d2101b1b1003
remko janse <remko.janse@in10.nl>
2017-11-17 15:22:00 +0100

    Starting Adapter Pattern for devices

---
+++ app/Events/DeviceUpdated.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class DeviceUpdated
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+
+    /**
+     * The Device that changed
+     *
+     * @var null
+     */
+    public $device = null;
+
+    /**
+     * The New Device State
+     *
+     * @var null
+     */
+    public $newDeviceState = null;
+
+    /**
+     * The old Device State
+     *
+     * @var null
+     */
+    public $oldDeviceState = null;
+
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($device)
+    {
+        //
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
---
+++ app/Models/Devices/OutputDeviceAdapter.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Model\Devices;
+
+/**
+ * Class OutputDevice,
+ *
+ * @author Remko Janse  <remko@djneo.nl>
+ */
+class OutputDeviceAdapter
+{
+
+}
